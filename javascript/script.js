b681dceb24de906ef4a1f1c8c42cf1f8a82d15fd
g-beal <112448756+g-beal@users.noreply.github.com>
2022-11-20 21:59:52 -0600

    added map and map test

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,13 @@
+var map;
 function initMap() {
 
   var chi = { lat:41.8722, lng:87.6188 };
-  // The map, centered at Uluru
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+
+  map = new google.maps.Map(document.getElementById("map"), {
     center: chi,
+    zoom: 5,
   });
-  // The marker, positioned at Uluru
+
   var starbucks = new google.maps.Marker({
     position: { lat:41.8747, lng:87.6290 };
     map: map,
