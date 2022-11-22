7f1e1b3ec408a9e5bae7580166af56fa14467e64
g-beal <112448756+g-beal@users.noreply.github.com>
2022-11-21 21:06:23 -0600

    manually added from last commit on github desktop

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,10 @@
 var map;
 function initMap() {
 
-  var chi = { lat:41.8722, lng:87.6188 };
-
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: chi,
-    zoom: 5,
-  });
-
-  var starbucks = new google.maps.Marker({
-    position: { lat:41.8747, lng:87.6290 };
-    map: map,
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.8722, lng: 87.6188},
+    zoom: 8
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+window.initMap = initMap;
