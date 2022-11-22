66a7a07ae9d7002fb1737443c114769be19c53a3
g-beal <112448756+g-beal@users.noreply.github.com>
2022-11-21 22:14:26 -0600

    added details to map

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,38 @@ var map;
 function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.8722, lng: 87.6188},
-    zoom: 8
+    center: {lat: 41.8741, lng: -87.6285},
+    zoom: 17
   });
+
+  var school = new google.maps.Marker({
+    position: { lat: 41.8730, lng: -87.6279},
+    map: map
+  });
+
+  var coffee = new google.maps.Marker({
+    position: { lat: 41.87470985506898, lng: -87.62902108075652 },
+    map: map
+  });
+
+  var distPoints = [
+    { lat: 41.8730, lng: -87.6279 },
+    { lat: 41.87299780452928, lng: -87.62762881329594 },
+    { lat: 41.874529665029925, lng: -87.62765288743249 },
+    { lat: 41.87455580971936, lng: -87.6290349675567 },
+    { lat: 41.87470985506898, lng: -87.62902108075652 },
+  ];
+
+  var dist = new google.maps.Polyline({
+    path: distPoints,
+    geodesic: true,
+    strokeColor: "#FE5F55",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  dist.setMap(map);
+
 }
 
 window.initMap = initMap;
